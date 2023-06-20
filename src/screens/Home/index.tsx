30277dcaad95961bdc19aa80a8c6105759f9da0c
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-06-20 16:37:20 -0300

    feat: adicionado alertas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,30 @@
-import { Text, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View, FlatList, Alert } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
 export function Home() {
   const participants = ['Lucas', 'Vini', 'Maria', 'John', 'Iza', 'Saori', 'Francisco', 'Julia', 'Antonio']
 
-  function handleParticipantAdd() {
-    console.log('Clicou')
+  function handleParticipantAdd(name: string) {
+    if (participants.includes(name)) {
+      return Alert.alert("Participante já existe na lista","Por favor, adicione um participante com outro nome")
+    }
   }
 
-  function handleParticipantRemove() {
-    console.log('Removido')
+  function handleParticipantRemove(name: string) {
+     Alert.alert("Remover participante",`Remover o participante ${name}`,
+     [
+     {
+      text: 'Sim',
+      onPress: () => Alert.alert('Excluído', `${name} foi removido da lista`)
+     },
+     {
+      text: 'Não',
+      style: 'cancel'
+     }
+     ]
+     )
+
   }
 
   return (
@@ -25,7 +39,7 @@ export function Home() {
           placeholderTextColor='#6B6B6B'
         />
 
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <TouchableOpacity style={styles.button} onPress={() => handleParticipantAdd('Lucas')}>
           <Text style={styles.buttonText}>
             +
           </Text>
@@ -37,7 +51,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Participant
             name={item}
-            onRemove={handleParticipantRemove}
+            onRemove={() => handleParticipantRemove(item)}
           />
         )}
         showsVerticalScrollIndicator={false}
